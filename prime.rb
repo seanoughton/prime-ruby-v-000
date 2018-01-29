e9140ef9d0047335086655a1497cd634a4b7c45f
Sean Oughton <seanoughton@gmail.com>
2018-01-29 18:52:01 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,14 @@
 # Add  code here!
 def prime?(number)
-  if number<= 1
+  if number <= 1
     return false
   end
   number_list = (2..number-1).to_a
-  number_list.each do |x|
-    remainder = number%x
+  number_list.each do
+    remainder = (number % x)
     if remainder == 0
       return false
+    end
   end
   return true
 end
