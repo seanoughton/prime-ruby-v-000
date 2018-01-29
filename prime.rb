6562b11c98ff3bc72c6149d9c2e8c602b06864b1
Sean Oughton <seanoughton@gmail.com>
2018-01-29 18:01:17 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,16 @@
 # Add  code here!
 def prime?(number)
+  if number <= 1
+    return false
+  end
+  idx= 2
+  while idx < number
+      remainder = number%idx
+      puts remainder
+      if remainder == 0 || number == 2
+          return false
+      end
+      idx += 1
+  end
+  return true
 end
