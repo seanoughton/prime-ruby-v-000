374daaf3b510f012c035130538f931738c9b5612
Sean Oughton <seanoughton@gmail.com>
2018-01-29 18:47:18 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,14 +3,11 @@ def prime?(number)
   if number <= 1
     return false
   end
-  idx= 2
-  while idx < number
-      remainder = number%idx
-      puts remainder
-      if remainder == 0 || number == 2
-          return false
-      end
-      idx += 1
+  number_list = (2..number-1).to_a
+  number_list.each do |x|
+    remainder = number%x
+      return false
+    end
   end
   return true
 end
